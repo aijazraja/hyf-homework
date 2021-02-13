71911a278912753eef60a8d25ca76276b234ae41
aijazraja <bazaaraijaz@gmail.com>
2021-02-13 19:05:23 +0100

    Fixed comments given by Axel Ekamba

--- nodejs/week2/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/meal-sharing/src/backend/api/meals-router.js
@@ -21,32 +21,44 @@ router.get("/:id", async (request, response) => {
 router.get("/", async (request, response) => {
   
   try {
+    if(request.query.maxPrice){
+      const maximumPrice=parseInt(request.query.maxPrice) > 0 ? parseInt(request.query.maxPrice) : 0;
+      if(maximumPrice>0){
+        const mealsWithMaxPrice=meals.filter((oneMeal)=>oneMeal.price<=maximumPrice);
+        response.json(mealsWithMaxPrice);
+      }else
+       response.send("Invalid max price");
+      
+    }else if(request.query.title){
+      
+      const titleOfMeal=request.query.title.toLowerCase();
+      if(titleOfMeal!=''){
+        const mealWithTitle=meals.filter(oneMeal=>oneMeal.title.toLowerCase().includes(titleOfMeal));
+        response.json(mealWithTitle);
+      }else
+        response.send('title is empty')
+      
 
-    response.send(meals); 
-    //Showing all meals, plese uncomment down to see different meal options.
- 
-    const mxPrice=parseInt(request.query.maxPrice) > 0 ? parseInt(request.query.maxPrice) : 0;
-    const mealsWithMaxPrice=meals.filter((oneMeal)=>oneMeal.price<=mxPrice);
-    //response.json(mealsWithMaxPrice);
+    }else if(request.query.createdAfter){
+      const dateOfMeals=new Date(request.query.createdAfter);
+      if(!isNaN(dateOfMeals)) {
+        const mealsWithMaxDate= meals.filter(oneMeal=>new Date(oneMeal.createdAt)>dateOfMeals);
+         response.json(mealsWithMaxDate);
 
-
-    /* 
-
-    const titleofMeal=request.query.title.toLowerCase();
-    const mealWithTitle=meals.filter(oneMeal=>oneMeal.title.toLowerCase().includes(titleofMeal));
-    response.json(mealWithTitle);
-
-    */
-    
-
-    const dateOfMeals=new Date(request.query.createdAfter);
-    const mealsWithMaxDate= meals.filter(oneMeal=>new Date(oneMeal.createdAt)>dateOfMeals);
-    //response.json(mealsWithMaxDate)
-
-
-    const limit1=parseInt(request.query.limit);
-    const mealsWithLimit=meals.slice(0,limit1);
-    //response.json(mealsWithLimit);
+      }else
+       response.send('Invalid date');
+    } else if(request.query.limit)
+    {
+      const limitOfMeals=parseInt(request.query.limit);
+      if(limitOfMeals>0){
+        const mealsWithLimit=meals.slice(0,limitOfMeals);
+        response.json(mealsWithLimit);
+      }else
+      response.send('Invalid limit');
+      
+    }
+    else
+    response.json(meals);
 
   } catch (error) {
     throw error;
